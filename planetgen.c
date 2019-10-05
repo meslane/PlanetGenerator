5c06f7fc5bb0cd146dcf30d0e2582f0fb833f762
meslane <merrickslane@gmail.com>
2019-10-05 10:00:02 -0700

    added clouds and optimized some things

--- planetgen.c
+++ planetgen.c
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <time.h>
 
-#pragma pack(push, 1) /* removes padding from sructs */
+#pragma pack(push, 1) /* removes padding from structs */
 
 int rng(const int min, const int max) {
     return (rand() % (max + 1 - min)) + min;
@@ -81,51 +81,56 @@ void generateBitmap(int width, int height, float dpi, const char* filename, pixe
 	fclose(bitmap);
 }
 
-void generateMap(int xSize, int ySize, int max, int seaLevel, int grad) {
+double averageOfNeighbors (char** array, int xSize, int ySize, int x, int y, char r) {
+	int xOffset, yOffset;
+	double sum = 0;
+	int members = 0;
+	
+	if ((x - r >= 0) && (y - r >= 0) && (x + r < xSize) && (y + r < ySize)) {
+		for (xOffset = -r; xOffset <= r; xOffset++) {
+			for (yOffset = -r; yOffset <= r; yOffset++) {
+				sum = sum + array[x + xOffset][y + yOffset];
+				members++;
+			}
+		}
+	}
+	
+	return (sum / members);
+}
+
+void generateMap(char* filename, int size, char max, char seaLevel, char grad) {
 	char** randmap;
-	char** map;
+	char** cloudLayer;
 	int i;
 	int x, y;
-	int nXOffset, nYOffset;
-	double sum;
-	int sumIncs;
+	int avg;
 	int p = 0;
 	pixel* imData;
 	
-	imData = (pixel*) malloc(xSize * ySize * sizeof(pixel));
+	imData = (pixel*) malloc(pow(size, 2) * sizeof(pixel));
 	
-	randmap = (char**) malloc(xSize * sizeof(char*));
-	map = (char**) malloc(xSize * sizeof(char*));
-	for (i = 0; i < ySize; i++) {
-		map[i] = (char*) malloc(ySize * sizeof(char));
-		randmap[i] = (char*) malloc(ySize * sizeof(char));
+	randmap = (char**) malloc(size* sizeof(char*));
+	for (i = 0; i < size; i++) {
+		randmap[i] = (char*) malloc(size * sizeof(char));
 	}
 	
-	for (x = 0; x < xSize; x++) { /* assign random values */
-		for (y = 0; y < ySize; y++) {
+	for (x = 0; x < size; x++) { /* assign random values */
+		for (y = 0; y < size; y++) {
 			randmap[x][y] = rng(0, max);
 		}
 	}
 	
-	for (x = 0; x < xSize; x++) { /* average pixels by neighbors */ 
-		for (y = 0; y < ySize; y++) {
-			sum = 0;
-			sumIncs = 0;
-			if ((x - grad >= 0) && (y - grad >= 0) && (x + grad < xSize) && (y + grad < ySize)) {
-				for (nXOffset = -grad; nXOffset <= grad; nXOffset++) {
-					for (nYOffset = -grad; nYOffset <= grad; nYOffset++) {
-						sum = sum + randmap[x + nXOffset][y + nYOffset];
-						sumIncs++;
-					}
-				}
-			}
-			map[x][y] = floor(sum / sumIncs);
-			if (pow((x - xSize/2), 2) + pow((y - ySize/2), 2) < pow((xSize + ySize) / 4 - grad, 2)) {
-				if (map[x][y] <= seaLevel) {
+	for (x = 0; x < size; x++) { /* average pixels by neighbors */ 
+		for (y = 0; y < size; y++) {
+			
+			avg = floor(averageOfNeighbors(randmap, size, size, x, y, grad));
+			
+			if (pow((x - size / 2), 2) + pow((y - size / 2), 2) < pow(size / 2 - grad, 2)) {
+				if (avg <= seaLevel) {
 					imData[p].b = 128; /* draw ocean */
 				}
 				else {
-					imData[p].g = map[x][y] * 20;
+					imData[p].g = avg * 20;
 				}
 			}
 			else { /* fill in outside of planet circle with space + stars */
@@ -145,9 +150,9 @@ void generateMap(int xSize, int ySize, int max, int seaLevel, int grad) {
 	}
 	
 	for (i = 0; i < 2; i++){
-		for (p = 0; p < (xSize * ySize); p++) { /* fill lone pixels with ocean/land */
+		for (p = 0; p < pow(size, 2); p++) { /* fill lone pixels with ocean/land */
 			if (imData[p].g != 0 && imData[p].g != 255) {
-				if (imData[p - xSize].b == 128 && imData[p + xSize].b == 128) {
+				if (imData[p - size].b == 128 && imData[p + size].b == 128) {
 					imData[p].b = 128;
 					imData[p].g = 0;
 				}
@@ -158,7 +163,7 @@ void generateMap(int xSize, int ySize, int max, int seaLevel, int grad) {
 			}
 			if (imData[p].b != 0 && imData[p].b != 255) {
 				if (imData[p - 1].g != 0 && imData[p + 1].g !=0 && 
-				imData[p - xSize].g != 0 && imData[p + xSize].g != 0 ) {
+				imData[p - size].g != 0 && imData[p + size].g != 0 ) {
 					imData[p].b = 0;
 					imData[p].g = imData[p - 1].g;
 				}
@@ -166,20 +171,91 @@ void generateMap(int xSize, int ySize, int max, int seaLevel, int grad) {
 		}
 	}
 	
-	generateBitmap(xSize, ySize, 0, "map.bmp", imData);
+	generateBitmap(size, size, 0, filename, imData);
 	free(imData);
 	
-	for (i = 0; i < ySize; i++) {
-		free(map[i]);
+	for (i = 0; i < size; i++) {
 		free(randmap[i]);
 	}
 	free(randmap);
-	free(map);
+}
+
+void overlayClouds(char* filename, int size, char cloudMax, char cloudLevel, char cloudGrad) {
+	char** randClouds;
+	unsigned char pixelData[3]; /* b, g, r */
+	int i;
+	int x, y;
+	char colors[3];
+	char cloudCleared;
+	
+	FILE* f;
+	f = fopen(filename, "rb+");
+	
+	randClouds = (char**) malloc(pow(size, 2) * sizeof(char*));
+	for (i = 0; i < size; i++) {
+		randClouds[i] = (char*) malloc(size * sizeof(char));
+	}
+	
+	for (y = 0; y < size; y++) {
+		for (x = 0; x < size; x++) {
+			randClouds[x][y] = rng(0, cloudMax);
+		}
+	}
+	
+	fseek(f, 54, SEEK_SET); /* move pointer past header */
+	
+	for (y = 0; y < size; y++) {
+		for (x = 0; x < size; x++) {
+			fread(pixelData, 3, 1, f);
+			if (pixelData[0] == 128 || (pixelData[1] != 0 && pixelData[1] != 255)) { /* if over planet */
+				cloudCleared = 0;
+				
+				if (pixelData[2] == 220) { /* clear previous clouds */
+					if (pixelData[0] == 220) {
+						colors[2] = 0;
+						colors[1] = pixelData[1] - 75;
+						colors[0] = 0;
+					}
+					else if (pixelData[1] == 220) {
+						colors[2] = 0;
+						colors[1] = 0;
+						colors[0] = 128;
+					}
+					fseek(f, -3, SEEK_CUR);
+					fwrite(colors, 3, 1, f);
+					cloudCleared = 1;
+				}
+				
+				if (averageOfNeighbors(randClouds, size, size, x, y, cloudGrad) >= cloudLevel) {
+					if (cloudCleared == 1) {
+						fseek(f, -3, SEEK_CUR); /* get new img data as opposed to the old */
+						fread(pixelData, 3, 1, f);
+					}
+					
+					if (pixelData[1] != 0) {
+						colors[2] = 220;
+						colors[1] = pixelData[1] + 75;
+						colors[0] = 220;
+					}
+					else {
+						colors[2] = 220;
+						colors[1] = 220;
+						colors[0] = 235;
+					}
+					
+					fseek(f, -3, SEEK_CUR);
+					fwrite(colors, 3, 1, f);
+				}
+			}
+		}
+		fseek(f, (size % 4), SEEK_CUR); /* skip padding bytes */
+	}
+	fclose(f);
 }
 
 int main(void) {
 	srand(time(0));
-	generateMap(127, 127, 16, 7, 8);
-	
+	generateMap("map.bmp", 127, 16, 7, 8);
+	overlayClouds("map.bmp", 127, 16, 9, 3);
 	return 0;
 }
